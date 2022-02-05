f618a190e5c81cbf0e9ac4f4159800e9814cc0c4
profi <profi@DESKTOP-695H3QE>
2022-02-05 11:34:15 +0100

    Creamos el enum Tramo

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Tramo.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio;
+
+
+public enum Tramo {
+MANANA, TARDE;
+	
+	private Tramo() {
+		
+	}
+	public String toString() {
+		return null;
+	}
+}
+
